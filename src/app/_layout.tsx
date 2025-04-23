7e3b7f0b11edad631947c221ed61c9315ea96864
Daniel Boris Limachi Cuiza <daniel.boris.limachi.cuiza@gmail.com>
2025-04-23 01:03:13 -0400

    refactor(layout): inicializar el estado de autenticación y envolver el layout en QueryClientProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,16 +2,21 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import { useFrameworkReady } from "@/hooks/use-framework-ready";
+import { useAuthStore } from "@/store/auth";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-client";
+
+useAuthStore.getState().initialize();
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </QueryClientProvider>
   );
 }
