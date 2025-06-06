331f66d916d22cefd3910458085f7fb54fe06021
cesar <cesarcrac44@gmail.com>
2025-06-05 21:26:55 -0400

    se actualzio el uso del socker y control al actualizar un plato

--- src/app/[idmesa]/bebidas_usuario.tsx
+++ src/app/[idmesa]/bebidas_usuario.tsx
@@ -6,7 +6,8 @@ import Section from "@/shared/components/ui/section";
 import { useLocalSearchParams } from "expo-router";
 import { DISH_TYPES } from "@/features/dish/constants";
 import { getDishCategory } from "@/features/dish/utils";
-import { setupDishListeners } from "@/shared/lib/socket/socketListeners";
+import { createDishSocket } from "@/features/dish/socket";
+import { groupBy } from "@/shared/lib/utils";
 export const BebidaScreenUsuario = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
@@ -18,30 +19,38 @@ export const BebidaScreenUsuario = () => {
     type: DISH_TYPES.DRINK.value,
     isAvailable: esCliente,
   });
+  const { onCreated, onUpdated, onDeleted, cleanup } = createDishSocket();
   useEffect(() => {
-    const { onCreated, onUpdated, onDeleted, cleanup } = setupDishListeners();
-
-    onCreated((newDish) => setDishes((prev = []) => [...prev, newDish]));
+    onCreated((newDish) => {
+      setDishes((prev) => [...(prev ?? []), newDish]);
+    });
 
     onUpdated((updatedDish) =>
-      setDishes((prev = [updatedDish]) =>
-        prev.map((dish) => (dish.id === updatedDish.id ? updatedDish : dish))
+      setDishes((prev = []) =>
+        !updatedDish.isAvailable
+          ? prev.filter((dish) => dish.id !== updatedDish.id)
+          : prev.some((dish) => dish.id === updatedDish.id)
+            ? prev.map((dish) =>
+                dish.id === updatedDish.id ? updatedDish : dish
+              )
+            : [...prev, updatedDish]
       )
     );
 
-    onDeleted(({ id }) =>
-      setDishes((prev = []) => prev.filter((dish) => dish.id !== id))
-    );
+    onDeleted(({ id }) => {
+      setDishes((prev) => (prev ?? []).filter((dish) => dish.id !== id));
+    });
 
-    return cleanup;
+    return () => {
+      cleanup();
+    };
   }, []);
-
   // control de respuesta de la API
   if (isLoading) return <Text>Cargando...</Text>;
   if (error) return <Text>Error al cargar los platos: {error.message}</Text>;
   // Asegúrate de que dishes no sea undefined, null o está vacío
   if (!dishes?.length) return <Text>No hay platos disponibles</Text>;
-  const grouped = Object.groupBy(dishes, (dish) => dish.category);
+  const grouped = groupBy(dishes, (dish) => dish.category);
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
--- src/app/[idmesa]/menu_usuario.tsx
+++ src/app/[idmesa]/menu_usuario.tsx
@@ -1,13 +1,13 @@
 import Header from "@/shared/components/ui/header";
 import { useGetDishes } from "@/features/dish/hooks";
 import Section from "@/shared/components/ui/section";
-import { useRouter } from "expo-router";
 import React, { useState, useEffect } from "react";
 import { ScrollView, View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { DISH_TYPES } from "@/features/dish/constants";
 import { getDishCategory } from "@/features/dish/utils";
-import { setupDishListeners } from "@/shared/lib/socket/socketListeners";
+import { createDishSocket } from "@/features/dish/socket";
+import { groupBy } from "@/shared/lib/utils";
 export const MenuScreenUsuario = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
@@ -20,31 +20,38 @@ export const MenuScreenUsuario = () => {
     isAvailable: esCliente,
   });
 
-  console.log(idmesa);
+  const { onCreated, onUpdated, onDeleted, cleanup } = createDishSocket();
   useEffect(() => {
-    const { onCreated, onUpdated, onDeleted, cleanup } = setupDishListeners();
-
-    onCreated((newDish) => setDishes((prev = []) => [...prev, newDish]));
+    onCreated((newDish) => {
+      setDishes((prev) => [...(prev ?? []), newDish]);
+    });
 
     onUpdated((updatedDish) =>
-      setDishes((prev = [updatedDish]) =>
-        prev.map((dish) => (dish.id === updatedDish.id ? updatedDish : dish))
+      setDishes((prev = []) =>
+        !updatedDish.isAvailable
+          ? prev.filter((dish) => dish.id !== updatedDish.id)
+          : prev.some((dish) => dish.id === updatedDish.id)
+            ? prev.map((dish) =>
+                dish.id === updatedDish.id ? updatedDish : dish
+              )
+            : [...prev, updatedDish]
       )
     );
 
-    onDeleted(({ id }) =>
-      setDishes((prev = []) => prev.filter((dish) => dish.id !== id))
-    );
+    onDeleted(({ id }) => {
+      setDishes((prev) => (prev ?? []).filter((dish) => dish.id !== id));
+    });
 
-    return cleanup;
+    return () => {
+      cleanup();
+    };
   }, []);
-
   // control de respuesta de la API
   if (isLoading) return <Text>Cargando...</Text>;
   if (error) return <Text>Error al cargar los platos: {error.message}</Text>;
   // Asegúrate de que dishes no sea undefined, null o está vacío
   if (!dishes?.length) return <Text>No hay platos disponibles</Text>;
-  const grouped = Object.groupBy(dishes, (dish) => dish.category);
+  const grouped = groupBy(dishes, (dish) => dish.category);
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
