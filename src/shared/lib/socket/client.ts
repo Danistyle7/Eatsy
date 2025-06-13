70ccb32bc0316f7f8c45ff31e88e7cc6e72bc57c
Daniel Li <129468193+daniel2010001@users.noreply.github.com>
2025-06-13 18:52:41 -0400

    Refactor API and Socket client configurations
    - Removed local dependency on "eatsy-app" from package.json.
    - Updated API client to use a hardcoded production base URL instead of environment variable.
    - Changed Socket client to connect to a hardcoded production WebSocket URL, removing reliance on environment variables.

--- src/shared/lib/socket/client.ts
+++ src/shared/lib/socket/client.ts
@@ -1,11 +1,9 @@
 import { io, Socket } from "socket.io-client";
 
 // TODO: mover a core/config
-const env = process.env;
-const SOCKET_URL = env.EXPO_PUBLIC_SOCKET_URL || "ws://localhost:8000";
 
 export const createSocket = (): Socket =>
-  io(SOCKET_URL, {
+  io("wss://eatzybe-production.up.railway.app/", {
     transports: ["websocket"],
     autoConnect: true,
     reconnectionAttempts: 5,
